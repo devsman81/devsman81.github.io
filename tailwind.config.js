74a948ef9189a9eafedef19152c88c8730deb04c
devsman81 <kur.sman81@gmail.com>
2023-05-15 14:15:26 +0700

    hehe

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#0e5db2',
+        dark: '#0f172a',
+
+      }
+    },
+  },
+  plugins: [],
+}
+
